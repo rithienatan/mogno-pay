8682998e4b9e4cfba536687f761b0880961d2c91
LarissaLanes <llarissalanep@gmail.com>
2022-04-30 20:21:32 -0300

    LandingPage base

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import "../footer/style.css"
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            footer
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,20 @@
+import Header from "../components/header/Header"
+import Footer from "../components/footer/Footer"
+import BenefitsMogno from "../components/landing-page/BenefitsMogno";
+import InformationsMogno from "../components/landing-page/InformationsMogno"
+import MognoApresentation from "../components/landing-page/MognoApresentation"
+
+
+const LandingPage = () => {
+    return(
+        <div>
+            <Header/>
+            <MognoApresentation/>
+            <InformationsMogno/>
+            <BenefitsMogno/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default LandingPage;
