fc223955d4af1d463d365c60d3de2fb857d22407
rithienatan <rithienatan@gmail.com>
2022-05-01 00:13:02 -0300

    Create branch sql codes and queries tables.

---
+++ TableCarteira.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Carteira(
+	id_cliente integer NOT NULL,
+	nome varchar(50) NOT NULL,
+	numero integer NOT NULL,
+	validade date NOT NULL,
+	cvv integer NOT NULL,
+	cardType varchar(10) NOT NULL,
+	carteiraHash varchar(64) NOT NULL PRIMARY KEY;
+);
---
+++ TableCliente.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Cliente(
+	id_cliente serial PRIMARY KEY NOT NULL, 
+	nome varchar(50) NOT NULL,
+	cpf varchar(14) NOT NULL,
+	email varchar(25) NOT NULL,
+	FOREIGN KEY(id_cliente) REFERENCES Carteira(id_cliente)
+);
