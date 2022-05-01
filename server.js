515acf259b8f5fbb7dc8f848ec4eebab2c8aaac7
rithienatan <rithienatan@gmail.com>
2022-05-01 16:05:49 -0300

    Bux fix 0.1.2 in route /getHash

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
+/**
+ * Mogno Pay API web service
+ * 
+ * @version 0.2.0
+ */
+//---------- imports ----------
 const express = require("express");
 const cors = require("cors");
+
+//---------- Custom imports ----------
+const GenerateHash = require("./aux-functions/GenerateHash");
+
+//---------- configs -----------
 const app = express();
-
-app.use(cors());
-app.get("/", (req, res) => res.json("Hello From AWS Beanstalk..."));
-
 const PORT = process.env.PORT || 5000;
+app.use(cors());
+
+//---------- routes ----------
+app.get("/", (req, res) => res.json("Api created for product MognoPay!"));
+
+app.get("/getHash", (req, res) => {
+    return res.json({
+        "hash": req.params,
+        "statusCode": 200
+    });
+});
+
+//----------Start server ----------
 app.listen(PORT, () => console.log(`Server started on port - ${PORT}`));
