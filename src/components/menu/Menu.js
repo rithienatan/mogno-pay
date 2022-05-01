06249095a3db4402d4f815846d0fcf6c6d6cee01
LarissaLanes <llarissalanep@gmail.com>
2022-05-01 14:06:06 -0300

    menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,9 +1,25 @@
-import "../header/style.css";
+import { Link } from "react-router-dom";
+import "./style.css";
+import logo from "../../assets/logo.png"
+import menuImage from "../../assets/enter-button.png"
 
 const Menu = () => {
     return(
-        <div className="header">
-            menuu
+        <div className="menu">
+                <img src={logo} id="logo"/>
+                <ul>
+                    <li>
+                        <button>fatura</button>
+                    </li>
+                    <li>
+                        <button>cashback</button>
+                    </li>
+                    <li>
+                        <button>lojas parceiras</button>
+                    </li>
+                </ul>
+                <img src={menuImage}/>
+
         </div>
     )
 }
