77d99422fcc464980315cd835bf9227ed4457ed6
Rithie Natan <rithienatan@gmail.com>
2022-05-01 17:12:40 -0300

    Update TableCliente.sql

--- TableCliente.sql
+++ TableCliente.sql
@@ -2,7 +2,7 @@ CREATE TABLE Cliente(
 	id_cliente serial PRIMARY KEY NOT NULL, 
 	nome varchar(50) NOT NULL,
 	cpf varchar(14) NOT NULL,
-    senha varchar(50) NOT NULL,
+    	senha varchar(50) NOT NULL,
 	email varchar(25) NOT NULL,
 	FOREIGN KEY(id_cliente) REFERENCES Carteira(id_cliente)
 );
