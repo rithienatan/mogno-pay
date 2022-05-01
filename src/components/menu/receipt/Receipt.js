79dae8a7e74cdb967af5b0cfe08c1fc094677f4d
Kaled Barreto <kaledbarreto@gmail.com>
2022-05-01 18:03:45 -0300

    Receipt

--- src/components/menu/receipt/Receipt.js
+++ src/components/menu/receipt/Receipt.js
@@ -1,12 +1,19 @@
 import Menu from '../Menu';
+import "./style.css";
 
 const Receipt = () => {
     return (
         <div className="receipt">
             <Menu />
-            Receipt
+            <div className='receipt-container'>
+                <div className='receipt-months'>
+                    <div className='month'>
+                        <span>dez/22</span>
+                        <span>R$ 65,88</span>
+                    </div>
+                </div>
+            </div>
         </div>
-
     )
 }
 
