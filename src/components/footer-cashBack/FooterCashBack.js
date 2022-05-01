a5c41106b25c1806dc07b20857d61e8338f3a90c
LarissaLanes <llarissalanep@gmail.com>
2022-05-01 17:57:07 -0300

    finish-page-cashBack

---
+++ src/components/footer-cashBack/FooterCashBack.js
@@ -0,0 +1,15 @@
+import "./style.css"
+
+const FooterCashBack = () => {
+    return(
+        <div className="footerCashBack">
+            <div>
+            <h1>Como você ajuda a neutralizar as emissões de CO2 com Mogno Pay</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempus elementum purus, vel porttitor tellus semper non. Aliquam erat volutpat. </p>
+            </div>
+        </div>
+
+    )
+}
+
+export default FooterCashBack;
--- src/components/menu/cashback/cashback.js
+++ src/components/menu/cashback/cashback.js
@@ -1,10 +1,36 @@
+import FooterCashBack from '../../footer-cashBack/FooterCashBack';
+import Table from '../../table/Table';
 import Menu from '../Menu';
+import "./style.css"
 
 const Cashback = () => {
     return (
-        <div className="receipt">
+        <div className="cash">
             <Menu />
-            Cashback
+            <div className="page-cash">
+                <h3 id="text-relatorio">Relatório Segunda Verde</h3>
+                <div className="contribution">
+                    <article id="frame-one">
+                        <p>Sua contribuição foi de</p>
+                        <h1>R$ 16,88</h1>
+                        <p>Nos últimos 7 dias</p>
+                    </article>
+                    <article id="frame-two">
+                        <p>Juntos somos mais fortes.</p>
+                        <h1>9 toneladas</h1>
+                        <p>de emissões de CO2 neutralizadas com a sua ajuda</p>
+                    </article>
+                </div>
+                <div id="details">
+                    <h3 id="text-table">Detalhamento do cashback</h3>
+                    <div>
+                        <Table/>
+                    </div>
+                 
+                </div> 
+                <FooterCashBack/>
+
+            </div>           
         </div>
 
     )
