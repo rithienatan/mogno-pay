5500ef3819d0ed0e55f351fe910bf5e44cfb5f3f
rithienatan <rithienatan@gmail.com>
2022-05-01 13:50:50 -0300

    Delete index.js file from remote repository

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-
-app.use(cors());
-app.get("/", (req, res) => res.json("Hello From AWS Beanstalk..."));
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port - ${PORT}`));
