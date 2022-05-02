0e5c496235c14bf4a7de73ecc6177f8c6fa96f02
Kaled Barreto <kaledbarreto@gmail.com>
2022-05-01 21:16:54 -0300

    Footer Cashback

--- src/components/footer-cashBack/FooterCashBack.js
+++ src/components/footer-cashBack/FooterCashBack.js
@@ -1,14 +1,24 @@
 import "./style.css"
+import carbon from '../../assets/Carbon.png';
+import moss from '../../assets/Moss.png';
 
 const FooterCashBack = () => {
-    return(
+    return (
         <div className="footerCashBack">
             <div>
-            <h1>Como você ajuda a neutralizar as emissões de CO2 com Mogno Pay</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempus elementum purus, vel porttitor tellus semper non. Aliquam erat volutpat. </p>
+                <h1>Como você ajuda a neutralizar as emissões de CO2 com Mogno Pay</h1>
+                <ul>
+                    <li>Utilize o cartão de crédito, o cashback gerado em sua fatura será acumulado com demais clientes MoGno.
+                    </li>
+                    <li>Esse valor será convertido em crédito de carbono, projetos que combatem as mudanças climáticas e promovem desenvolvimento sustentável. </li>
+                </ul>
+                <h1>Empresas parceiras na transação</h1>
+                <div className="images-div">
+                    <img src={carbon} alt="Corbon" />
+                    <img src={moss} alt="Moss" />
+                </div>
             </div>
         </div>
-
     )
 }
 
